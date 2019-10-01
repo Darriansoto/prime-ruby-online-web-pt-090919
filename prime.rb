be77a7720e383e6c18f09ed4cda76bd85ee7b44a
Darrian Soto <ddog350@gmail.com>
2019-10-01 19:21:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 def prime?(number)
-  (2..(number - 1)).each do |x|
-    return false if x % number == 0
-  end
-  true
+  #(2..(number - 1)).each do |x|
+    # return false if x % number == 0
+  #end
+  #true
+  
+  
+  
 end
 
